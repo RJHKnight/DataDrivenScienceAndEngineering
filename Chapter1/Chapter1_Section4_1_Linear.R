ba8a570d72471057ac32737a425df461ca60175e
Richard Knight <rjhknight@gmail.com>
2021-06-23 12:36:05 +1000

    Chapter 1 Section 4 - Linear Regresion 3 ways.

---
+++ Chapter1/Chapter1_Section4_1_Linear.R
@@ -0,0 +1,37 @@
+library(ggplot2)
+library(MASS)
+set.seed(0)
+
+# Set up toy data ---------------------------------------------------------
+
+# Gradient
+x <- 3
+
+# Range
+a <- seq(-2, 2, by = 0.25)
+
+# Add noise
+b <- a * x + rnorm(length(a))
+
+# SVD for Linear Fit
+svd_results <- svd(a)
+x_tilda <- svd_results$v %*% solve(svd_results$d) %*% t(svd_results$u) %*% b
+
+raw_data <- data.frame(x = a, y = b, type = "noisy")
+
+results <- rbind(data.frame(x = a, y = x * a, type = "true value"),
+                 data.frame(x = a, y = x_tilda[1] * a, type = "linear fit"))
+
+ggplot(results, aes(x, y, colour = type)) + 
+  geom_line() + 
+  geom_point(data = raw_data)
+
+
+# Compare the 3 methods of Linear Regression ------------------------------
+
+x_tilda_1 <- svd_results$v %*% solve(svd_results$d) %*% t(svd_results$u) %*% b
+x_tilda_2 <- ginv(a) %*% b
+x_tilda_3 <- coef(lm(b ~ -1 + a))$a
+
+# Check all equal.
+(x_tilda_1 == x_tilda_2) & (x_tilda_3 == x_tilda_2)
--- Misc/Install.R
+++ Misc/Install.R
@@ -1,4 +1,8 @@
-if (!require("imager")) install.packages("imager")
+# Core Packages
 if (!require("tidyverse")) install.packages("tidyverse")
+if (!require("MASS")) install.packages("MASS")
+
+# Plotting and Visualisation
+if (!require("imager")) install.packages("imager")
 if (!require("patchwork")) install.packages("patchwork")
 if (!require("rgl")) install.packages("rgl")
