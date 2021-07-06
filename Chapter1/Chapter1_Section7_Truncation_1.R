ee56ff6b7693463cdeebff3912cb0f33ea6200b2
Richard Knight <rjhknight@gmail.com>
2021-07-06 16:22:32 +1000

    Chapter 1 Section 7 Part 1

---
+++ Chapter1/Chapter1_Section7_Truncation_1.R
@@ -0,0 +1,85 @@
+library(tidyverse)
+source("Misc/Utils.R")
+library(patchwork)
+
+# True matrix -------------------------------------------------------------
+
+t <- seq(-3, 3, by=0.01)
+
+u_true <- cbind(cos(17 * t) * exp(-t^2), sin(11 * t))
+s_true <- diag(c(2, 0.5))
+v_true <- cbind(sin(5 * t) * exp(-t^2), cos(13 * t))
+
+x <- u_true %*% s_true %*% t(v_true)
+
+plot_matrix(x)
+
+# Add noise ---------------------------------------------------------------
+
+sigma <- 1
+n <- nrow(x)
+m <- ncol(x)
+x_noisy <- x + sigma * matrix(rnorm(n * m), ncol = m)
+
+plot_matrix(x_noisy)
+
+svd_res <- svd(x_noisy)
+
+
+# Hard threshold with known sigma and square X ----------------------------
+
+cutoff <- (4/sqrt(3)) * sqrt(n) * sigma
+
+r <- max(which(svd_res$d > cutoff))
+x_clean <- svd_res$u[, 1:r] %*% diag(svd_res$d[1:r]) %*% t(svd_res$v[, 1:r])
+
+plot_matrix(x_clean)
+
+
+# Cumulative Energy Cutoff ------------------------------------------------
+
+cds <- cumsum(svd_res$d) / sum(svd_res$d)
+r_90 <- min(which(cds > 0.9))
+
+x_90 <- svd_res$u[, 1:r_90] %*% diag(svd_res$d[1:r_90]) %*% t(svd_res$v[, 1:r_90])
+plot_matrix(x_90)
+
+
+# Singular Value Plots ----------------------------------------------------
+
+singular_values <- data.frame(s = svd_res$d) %>% 
+  mutate(j = 1:n()) %>% 
+  mutate(cum_s = cumsum(s) / sum(s)) %>% 
+  mutate(type = case_when(
+    s > cutoff ~ "below_cutoff", 
+    j < r_90   ~ "below_r90",
+    TRUE       ~ "other"))
+
+base_plot <- ggplot(singular_values, aes(j, s)) + 
+  geom_line() + 
+  geom_point(aes(colour = type)) + 
+  scale_y_log10() + 
+  geom_hline(yintercept = cutoff, colour = "red", linetype = "dashed") + 
+  theme_bw() + 
+  theme(legend.position = "none")
+
+p1 <- base_plot + 
+  annotate("rect", xmin = -5, xmax = 100, ymin = 20, ymax = 200, alpha = .2) + 
+  ggtitle("Singular Values")
+
+p2 <- base_plot + 
+  scale_x_continuous(limit = c(-5, 100)) +
+  scale_y_log10(limits = c(20, 200)) + 
+  ggtitle("Singular Values - Zoomed")
+
+p3 <- ggplot(singular_values, aes(j, cum_s)) +
+  geom_line() + 
+  geom_point(aes(colour = type)) + 
+  annotate("segment", x = r_90, xend = r_90, y = -Inf, yend = 0.9) + 
+  annotate("segment", x = -Inf, xend = r_90, y = 0.9, yend = 0.9) +
+  theme_bw() + 
+  theme(legend.position = "bottom") + 
+  ggtitle("90% Cutoff")
+  
+
+(p1 + p2) / p3
--- Misc/Utils.R
+++ Misc/Utils.R
@@ -11,3 +11,17 @@ sphere <- function()
   
   return (list(x = x, y = y, z = z))
 }
+
+# Equivalent to imshow in matlab
+plot_matrix <- function(x)
+{
+  x_df <- data.frame(x) %>% 
+    mutate(y = 1:n()) %>% 
+    pivot_longer(names_to = "x", names_prefix = "X", values_to = "z", -y) %>% 
+    mutate(x = as.integer(x))
+  
+  ggplot(x_df, aes(x,y, fill = z)) + 
+    geom_raster()  + 
+    theme_bw() + 
+    scale_fill_viridis_c()
+}
