9e59ec766e1379afd621ce3b9d09e07cac090de3
Richard Knight <rjhknight@gmail.com>
2021-08-17 19:51:14 +1000

    Adding in control - need to look into the matlab place command.

--- Chapter8/Chapter8_Section7_Cartpole.R
+++ Chapter8/Chapter8_Section7_Cartpole.R
@@ -21,7 +21,7 @@ A <- matrix(c(
   ), ncol = 4, byrow = TRUE
 )
 
-B <- c(0, 1/M, 0, b*1/(M*l))
+B <- matrix(c(0, 1/M, 0, b*1/(M*l)), ncol = 1)
 
 geigen(A)
 Rank(ctrb(A, B))
@@ -37,3 +37,27 @@ for (i in 1:length(res$t))
   draw_cart_pendulum(res$y[i,], m, M, l)
   ani.pause()
 }
+
+
+# Now use place to define a K matrix so our system has the desired eigenvalues
+
+p <- matrix(c(-1.1, -1.2, -1.3, -1.4), ncol = 1)
+#k <- place(A,B,p)
+k <- c(-1.716, -6.6357, 156.932, 61.0714)
+eigen(A - B %*% k)
+w_r <- c(3,0,pi,0)
+
+u <- function(x)
+{
+  -k %*% (x-w_r)
+}
+
+res <- ode45(function(t, x) pend_cart(t,x,m,M,l,g,d,u(x)), 0, 15, x_0)
+
+for (i in 1:length(res$t))
+{
+  dev.hold()
+  draw_cart_pendulum(res$y[i,], m, M, l)
+  ani.pause()
+}
+
