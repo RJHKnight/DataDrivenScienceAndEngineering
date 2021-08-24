e57cbda63ae2670c2e096a8b7a63294ad7b72cce
Richard Knight <rjhknight@gmail.com>
2021-08-24 12:16:35 +1000

    Chapter 8 Section 7: Observability under different C.

---
+++ Chapter8/Chapter8_Section7_2b_Observability.R
@@ -0,0 +1,45 @@
+library(control)
+library(pracma)
+library(tidyverse)
+
+m <- 1
+M <- 5
+l <- 2
+g <- -10
+d <- 1
+
+# Pendulum down
+b <- -1
+
+A <- matrix(c(
+  0,          1,                0, 0,
+  0,       -d/M,          b*m*g/M, 0,
+  0,          0,                0, 1,
+  0, -b*d/(M*l), -b*(m+M)*g/(M*l), 0
+), ncol = 4, byrow = TRUE)
+
+B <- matrix(c(0, 1/M, 0, b*1/(M*l)), ncol = 1)
+
+# Only observable if x is measured due to translational invariance.
+
+C_full <- diag(4)
+
+test_one <- function(this_c)
+{
+  this_c <- matrix(this_c, nrow = 1)
+  rank <- Rank(obsv(A, this_c))
+  det <- det(obsv(A, this_c))
+  
+  return (data.frame(c = paste(this_c, collapse = ","), rank = rank, det = det))
+}
+
+map_dfr(array_branch(C_full, 1), test_one)
+
+
+# And if we omit x
+A <- A[-1, -1]
+B <- B[-1]
+
+C_full <- diag(3)
+
+map_dfr(array_branch(C_full, 1), test_one)
