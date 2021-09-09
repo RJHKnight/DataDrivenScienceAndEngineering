8d433f01c194f0a873d28f8185e18e960100720b
Richard Knight <rjhknight@gmail.com>
2021-09-09 18:30:30 +1000

    No hard coded values any more

--- Chapter8/Chapter8_Section7_1_LQR.R
+++ Chapter8/Chapter8_Section7_1_LQR.R
@@ -2,8 +2,10 @@ library(animation)
 library(QZ)
 library(control)
 library(pracma)
+library(tidyverse)
 source("Misc/CartPendulum.R")
 source("Misc/LQR.R")
+source("Misc/Place.R")
 
 ani.options(interval = 0.1)
 
@@ -45,9 +47,7 @@ for (i in 1:length(res$t))
 # Now use place to define a K matrix so our system has the desired eigenvalues
 
 p <- matrix(c(-1.1, -1.2, -1.3, -1.4), ncol = 1)
-#k <- t(place(A,B,p))
-k <- c(-1.716, -6.6357, 156.932, 61.0714)
-
+k <- t(place(A,B,p))
 
 eigen(A - B %*% k)
 w_r <- c(3,0,pi,0)
@@ -72,7 +72,7 @@ for (i in 1:length(res$t))
 q <- diag(c(1,1,1,1))
 r <- 0.0001
 
-k <- my_lqr(A, B, q, r)
+k <- my_lqr(A, B, q, r)$k
 
 
 eigen(A - B %*% k)
@@ -84,11 +84,117 @@ u <- function(x)
 }
 
 res <- NULL
-res <- ode45(function(t, x) pend_cart(t,x,m,M,l,g,d,u(x)), 0, 10, x_0)
+res <- ode45(function(t, x) pend_cart(t,x,m,M,l,g,d,u(x)), 0, 50, x_0)
+
+res_df <- data.frame(t = res$t, x = res$y[,1], x_dot = res$y[,2],
+                     theta = res$y[,3], theta_dot = res$y[,4])
+
+
+this_cost <- rep(0, length(res$t))
 
 for (i in 1:length(res$t))
 {
+  this_cost[i] <- (res$y[i,]-w_r) %*% q %*% (res$y[i,]-w_r) + u(res$y[i,])^2 %*% r
+}
+
+lqr_cost <- data.frame(
+  t      = res$t,
+  cost   = this_cost,
+  run_id = "lqr"
+)
+
+lqr_cost <- lqr_cost %>% 
+  arrange(t) %>% 
+  mutate(cost = cumtrapz(t,cost))
+
+
+res_df$time_bin <- cut(res_df$t, 100)
+
+# Convert to equal time steps
+y <- res_df %>% 
+  mutate(time_bin = cut(t, 100)) %>% 
+  group_by(time_bin) %>% 
+  summarise(
+    t = t[1],
+    x = mean(x),
+    x_dot = mean(x_dot),
+    theta = mean(theta),
+    theta_dot = mean(theta_dot)
+  ) %>% 
+  select(-time_bin) %>% 
+  as.matrix(.)
+  
+
+for (i in 1:length(y[,'t']))
+{
   dev.hold()
-  draw_cart_pendulum(res$y[i,], m, M, l)
+  draw_cart_pendulum(y[i,2:5], m, M, l)
   ani.pause()
 }
+
+
+# Comparison of LQR with various random pole placements
+
+state_df <- NULL
+cost_df <- NULL
+r <- 0.0001
+
+for (i in 1:100)
+{
+  # Random pole placement
+  p <- matrix(c(-.5-3*runif(1), -.5-3*runif(1), -.5-3*runif(1), -.5-3*runif(1)), ncol = 1)
+  k <- place(A,B,p)
+  
+  # Run simulation
+  res <- ode45(function(t, x) pend_cart(t,x,m,M,l,g,d,u(x)), 0, 50, x_0)
+  
+  # Store results
+  state_df <- rbind(state_df,
+                    data.frame(t         = res$t,
+                               x         = res$y[,1],
+                               x_dot     = res$y[,2],
+                               theta     = res$y[,3],
+                               theta_dot = res$y[,4],
+                               run_id    = as.character(i)))
+  
+  this_cost <- rep(0, length(res$t))
+  
+  for (i in 1:length(res$t))
+  {
+    this_cost[i] <- (res$y[i,]-w_r) %*% q %*% (res$y[i,]-w_r) + u(res$y[i,])^2 %*% r
+  }
+  
+  this_cost_df <- data.frame(
+    t      = res$t,
+    cost   = this_cost,
+    run_id = as.character(i)
+  )
+  
+  this_cost_df <- this_cost_df %>% 
+    arrange(t) %>% 
+    mutate(cost = cumtrapz(t,cost))
+  
+  cost_df <- rbind(cost_df, this_cost_df)
+}
+
+# Plot of state vs time
+state_df %>% 
+  pivot_longer(-c("t", "run_id"), "state", "value") %>% 
+  ggplot(aes(t, value, colour = run_id)) + 
+  geom_line() + 
+  facet_wrap(~ state, ncol = 2, scales = "free_y") + 
+  theme_bw() + 
+  theme(legend.position = "none")
+
+# Plot of cost vs time
+cost_df %>% 
+  filter(run_id %in% sample(unique(run_id), 10)) %>% 
+  ggplot(aes(t, cost, colour = run_id)) + 
+  geom_line(alpha = 0.6) +
+  geom_line(data = lqr_cost, colour = "black", size = 1.5, linetype = 2) +
+  theme_bw() + 
+  theme(legend.position = "none") + 
+  scale_x_continuous(limits = c(0, 10))
+
+
+
