3e0991a4dd78f4731e1d763c9e6c87381cfcf7c4
Richard Knight <rjhknight@gmail.com>
2021-09-15 15:17:58 +1000

    WIP

--- Chapter9/Chapter9_Section2_2_BalancedTruncation.R
+++ Chapter9/Chapter9_Section2_2_BalancedTruncation.R
@@ -1,16 +1,14 @@
 library(control)
 library(tidyverse)
 library(patchwork)
+library(rmatio)
 source("Misc/DRSS.R")
 source("Misc/Balred.R")
 
-q <- 2
-p <- 2
-n <- 100
 
-sys_full <- drss(n, p, q, strictly_proper = FALSE, type = "C")
+sys_full <- rmatio::read.mat("DATA/testSys_ABCD.mat")
 
-bal_sys <- balred(sys_full$A, sys_full$B, sys_full$C, sys_full$D, nrow(sys_full$B))
+bal_sys <- balred(sys_full$A, sys_full$B, sys_full$C,  nrow(sys_full$B))
 
 hsv_df <- data.frame(hsv = bal_sys$hsv) %>% 
   mutate(rank = 1:n()) %>% 
@@ -29,4 +27,33 @@ p2 <- ggplot(hsv_df, aes(rank, cum_hsv)) +
 p1 + p2
 
 r <- 10
-sys_bt <- balred(sys_full$A, sys_full$B, sys_full$C, sys_full$D, r)
+sys_bt <- balred(sys_full$A, sys_full$B, sys_full$C, r)
+
+
+
+# Compute BPOD ------------------------------------------------------------
+impulse_length <- 0:((r*5)+1)
+sys_full <- ss(sys_full$A, sys_full$B, sys_full$C, sys_full$D,1)
+u_impulse_1 <- matrix(0, ncol = 2, nrow = length(impulse_length))
+u_impulse_2 <- matrix(0, ncol = 2, nrow = length(impulse_length))
+u_impulse_1[1,1] <- 1
+u_impulse_2[1,2] <- 1
+
+y <- matrix(0, nrow = nrow(u_impulse), ncol = 2)
+x <- matrix(0, nrow = ncol(sys_full$A), 1)
+
+for (k in 1 : nrow(u_impulse))
+{
+  y[k,] <- t((sys_full$C %*% x)  +  sys_full$D %*% u_impulse_1[k,])
+  x <- (sys_full$A %*% x)  + (sys_full$B %*% u_impulse_1[k,])
+}
+
+
+lsim(sys_full, u_impulse_2, t = impulse_length)
+
+impulseplot(sys_full, impulse_length)
+
+sys_adj <- ss(t(sys_full$A), t(sys_full$C), t(sys_full$B), t(sys_full$D))
+impulse_adj <- impulse(sys_adj, seq(0, (r*5)+1, 0.01))
+
+# Hankel Matric H = OC
